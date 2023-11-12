2285e474d45eac98faad9f23ba3749ef98463084
Azeez Adebayo <debaycisse@gmail.com>
2023-11-12 20:52:15 +0100

    add header file

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct func - structure to match character with function
+ *
+ * @format: the format to match up, such as c for character
+ * @f: the specific function fo each format character
+ */
+typedef struct func
+{
+	char format;
+	void (*f)(char ch);
+} func_t;
+
+int _printf(const char *format, ...);
+
+
+#endif

0ec08d31fa2d04036404dec2f4cc7ac65b47a41d
Azeez Adebayo <debaycisse@gmail.com>
2023-11-14 16:46:16 +0100

    work in progress - task 0

--- 0-write_c.c
+++ 0-write_c.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <unistd.h>
 
+/**
+ * write_char - prints character to the console or screen
+ * @ch: the character to be printed
+ */
 void write_char(char ch)
 {
-	write(1, &ch, sizeof(char));
+	_putchar(ch);
 }
--- main.h
+++ main.h
@@ -5,16 +5,24 @@
  * struct func - structure to match character with function
  *
  * @format: the format to match up, such as c for character
- * @f: the specific function fo each format character
+ * @f_char: function to handle character format
+ * @f_str: function to handle string format
+ * @f_int: function to handle integer & decimal base 10 format
+ * @f_float: function to handle float format
  */
 typedef struct func
 {
 	char format;
-	void (*f)(char ch);
+	void (*f_char)(char);
+	void (*f_str)(char *);
+	void (*f_int)(int);
+	void (*f_float)(float);
 } func_t;
+
 
 int _printf(const char *format, ...);
 void write_char(char ch);
-
+void write_string(char *str);
+int _putchar(char ch);
 
 #endif

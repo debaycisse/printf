2fe7a03ecbace834199c7f3ba873a3cdb88d09ef
Azeez Adebayo <debaycisse@gmail.com>
2023-11-16 06:33:04 +0100

    work in progress - task 1

--- percent_sym.c
+++ percent_sym.c
@@ -10,32 +10,30 @@
  */
 void percent_symbol(char ch, int *count, va_list v_parameters)
 {
-	switch (ch)
+	if (ch == 'c')
 	{
-		case 'c':
-			write_char(va_arg(v_parameters, int));
-			(*count)++;
-			break;
-		case 's':
-			write_string(va_arg(v_parameters, char *));
-			(*count)++;
-			break;
-		case '%':
-			write_p('%');
-			(*count)++;
-			break;
-		case 'i':
-			write_int(va_arg(v_parameters, int));
-			(*count)++;
-			break;
-		case 'd':
-			write_int(va_arg(v_parameters, int));
-			(*count)++;
-			break;
-		default:
-			printf("%");
-			putchar(ch);
-			*count += 2;
-			break;
+		write_char(va_arg(v_parameters, int));
+		(*count)++;
+	}
+	if (ch == 's')
+	{
+		write_string(va_arg(v_parameters, char *));
+		(*count)++;
+	}
+	if (ch == '%')
+	{
+		write_p('%');
+		(*count)++;
+	}
+	if ((ch == 'i') || (ch == 'd'))
+	{
+		write_int(va_arg(v_parameters, int));
+		(*count)++;
+	}
+	else
+	{
+		putchar('%');
+		putchar(ch);
+		*count += 2;
 	}
 }

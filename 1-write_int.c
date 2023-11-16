f73b26160ab1477888d711f7949c9c5010214c29
Azeez Adebayo <debaycisse@gmail.com>
2023-11-16 02:21:48 -0800

    add implementation for negative integer value

--- 1-write_int.c
+++ 1-write_int.c
@@ -2,6 +2,10 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * write_int - prints integer to the console
+ * @num: the integer that is send to the standard output
+ */
 void write_int(int num)
 {
 	int num_len = 1;
@@ -10,29 +14,50 @@ void write_int(int num)
 	int *num_array;
 	int num_len_cpy;
 
-	while (num_cpy > 9)
-	{
-		num_cpy /= 10;
-		num_len++;
-	}
+	if (num_cpy > 0)
+		while (num_cpy > 9)
+		{
+			num_cpy /= 10;
+			num_len++;
+		}
+	if (num_cpy < 0)
+		while (num_cpy < -9)
+		{
+			num_cpy /= 10;
+			num_len++;
+		}
+	num_cpy = num;
 	num_array = malloc(num_len * sizeof(*num_array));
 	num_len_cpy = num_len;
 	if (num_array != NULL)
 	{
 		for (i = 0; i < num_len_cpy; i++)
 		{
-			if (num <= 9)
-				num_array[0] = num;
+			if ((num_cpy <= 9) && (num > 0))
+				num_array[0] = num_cpy;
+			if ((num_cpy >= -9) && (num < 0))
+				num_array[0] = num_cpy;
 			else
 			{
 				num_len--;
-				num_array[num_len] = num % 10;
-				num /= 10;
+				num_array[num_len] = num_cpy % 10;
+				num_cpy /= 10;
 			}
 		}
-		for (i = 0; i < num_len_cpy; i++)
+		if (num > 0)
 		{
-			write_char(num_array[i] + '0');
+			for (i = 0; i < num_len_cpy; i++)
+			{
+				write_char(num_array[i] + '0');
+			}
+		}
+		if (num < 0)
+		{
+			write_char('-');
+			for (i = 0; i < num_len_cpy; i++)
+			{
+				write_char((-(num_array[i])) + '0');
+			}
 		}
 		free(num_array);
 	}
